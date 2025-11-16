b255b8adeb6473d705fe4dd64fc4a7dec0a961c1
itsmeank <maredjnacer@gmail.com>
2025-11-16 15:17:23 +0100

    Update PerceptionForm.tsx

--- src/pages/PerceptionForm.tsx
+++ src/pages/PerceptionForm.tsx
@@ -4,16 +4,6 @@ import PerceptionHeader from "@/components/perception/PerceptionHeader";
 import MultiStepForm from "@/components/perception/MultiStepForm";
 
 const PerceptionForm: React.FC = () => {
-  return (
-    <CosmicBackground className="min-h-[100svh] px-4 py-10 sm:px-6">
-      <div className="mx-auto max-w-5xl">
-        <PerceptionHeader />
-        <div className="mt-6">
-          <MultiStepForm />
-        </div>
-      </div>
-    </CosmicBackground>
-  );
 };
 
 export default PerceptionForm;
